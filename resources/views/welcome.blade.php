70b8cd6f0ae7ae1fb0d9c6cc9d089778384dc3af
Jota Vares <joe@eternallynocturnal.com.com>
2016-04-29 09:20:54 -0400

    master commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                background-image: url('./images/ally.jpg');
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Open Sans Condensed', sans-serif;
+            }
+
+            .container {
+                text-align: right;
+                display: table-cell;
+            }
+
+            .content {
+                text-align: center;
+                background-color: #fff;
+                padding:15px;
+                border-radius: 10px;
+                opacity: .8;
+                display: inline-block;
+                margin-right: 10%;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">SmackMyBishop<br>
+                Makeup Tutorials by Alexandria Bishop  </div>
+            </div>
+        </div>
+    </body>
+</html>
